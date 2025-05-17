1bd7603e55defae8443e946aa2d03519fdfd61c6
vietnv-aixblock <viet@aixblock.io>
2025-05-17 09:59:23 +0700

    Update load_model.py

--- load_model.py
+++ load_model.py
@@ -18,18 +18,18 @@ login(token=hf_access_token)
 
 def _load():
     model_id = "black-forest-labs/FLUX.1-dev"
-    nf4_config = BitsAndBytesConfig(
-        load_in_4bit=True,
-        bnb_4bit_quant_type="nf4",
-        bnb_4bit_compute_dtype=torch.bfloat16,
-    )
-    model_nf4 = FluxTransformer2DModel.from_pretrained(
-        model_id,
-        subfolder="transformer",
-        quantization_config=nf4_config,
-        torch_dtype=torch.bfloat16,
-        # device_map=device,
-    )
+    # nf4_config = BitsAndBytesConfig(
+    #     load_in_4bit=True,
+    #     bnb_4bit_quant_type="nf4",
+    #     bnb_4bit_compute_dtype=torch.bfloat16,
+    # )
+    # model_nf4 = FluxTransformer2DModel.from_pretrained(
+    #     model_id,
+    #     subfolder="transformer",
+    #     quantization_config=nf4_config,
+    #     torch_dtype=torch.bfloat16,
+    #     # device_map=device,
+    # )
     # pipe_demo = FluxPipeline.from_pretrained(
     #     model_id,
     #     transformer=model_nf4,
